d34b5ed316be5d0a95452adfa71ccc664baf745f
Duy <115519213+DuyBeatrix@users.noreply.github.com>
2024-08-16 10:42:53 +0700

    Delete TestBackend directory

--- TestBackend/src/main/java/com/example/TestBackend/Repo/TinhTrangHocRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.TestBackend.Repo;
-
-import com.example.TestBackend.Entity.TinhTrangHoc;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TinhTrangHocRepo extends JpaRepository<TinhTrangHoc, Integer> {
-}
